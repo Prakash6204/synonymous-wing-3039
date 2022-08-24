1f015e5831d6c4a42bb8527ab9a8aee153d32e4d
sukanta255 <sukantap872@gmail.com>
2022-08-24 21:38:33 +0530

    vitamin product page created

---
+++ product/vitamin_product.js
@@ -0,0 +1,394 @@
+var fashionData = [
+    {
+      avatar:
+        "https://onemg.gumlet.io/images/c_fit,q_auto,w_150,f_auto,h_150/jmuuryvgit6mwwolt0ea/revital-h-capsule.jpg",
+      brand: "Revital H Capsule",
+      name: "bottle of 60 soft gelatin capsules",
+      rupees: "₹",
+      price: 495,
+      rupees2: "₹",
+      price2: 999,
+      offer: "10% OFF"
+  
+    },
+    {
+      avatar:
+        "https://onemg.gumlet.io/images/c_fit,q_auto,w_150,f_auto,h_150/gmkrpf1imjj9djwvq4gq/tata-1mg-multivitamin-supreme-zinc-calcium-and-vitamin-d-immunity-booster-capsule.jpg",
+  
+      brand: "Tata 1mg Multivitamin Supreme, Zinc, Calcium and Vitamin D",
+      name: "bottle of 60 capsules",
+      rupees: "₹",
+      price: 498,
+      rupees2: "₹",
+      price2: 995,
+      offer: "50% OFF",
+  
+    },
+    {
+      avatar:
+        "https://onemg.gumlet.io/images/c_fit,q_auto,w_150,f_auto,h_150/xbhkkhnpj6gsv3xldvwq/carbamide-forte-multivitamin-sports-vegetarian-tablet.jpg",
+      brand: "Carbamide Forte Multivitamin Sports Vegetarian Tablet",
+      name: "bottle of 60 tablets",
+      rupees: "₹",
+      price: 499,
+      rupees2: "₹",
+      price2: 875,
+      offer: "43% OFF"
+    },
+    {
+      avatar:
+        "https://onemg.gumlet.io/images/c_fit,q_auto,w_150,f_auto,h_150/cropped/omz1zuuelyaqnupl0wwe/maxirich-multivitamin-minerals-softgel.png",
+      brand: "Maxirich Multivitamin & Minerals Softgel",
+      name: "box of 30 soft gelatin capsules",
+      rupees: "₹",
+      price: 198,
+      rupees2: "₹",
+      price2: 330,
+      offer: "10% OFF"
+  
+    },
+    {
+      avatar:
+        "https://onemg.gumlet.io/images/c_fit,q_auto,w_150,f_auto,h_150/geiwxvovi2c600riqel3/wellman-health-supplement-for-men-tablet.jpg",
+      brand: "Wellman Health Supplement for Men Tablet",
+      name: "strip of 30 tablets",
+      rupees: "₹",
+      price: 350,
+      rupees2: "₹",
+      price2: 438,
+      offer: "20% OFF"
+    },
+    {
+      avatar:
+        "https://onemg.gumlet.io/images/c_fit,q_auto,w_150,f_auto,h_150/aywb3kusfcv1tnyyt8qy/dr.-morepen-multi-vitamin-men-with-omega-3-herbs-natural-energy-immunity-booster-tablet.jpg",
+      brand: "Dr. Morepen Multi Vitamin Men with Omega 3 & Herbs, Natural Energy &",
+      name: "bottle of 60 tablets",
+      rupees: "₹",
+      price: 749,
+      rupees2: "₹",
+      price2: 999,
+      offer: "25% OFF"
+    },
+    {
+      avatar:
+        "https://onemg.gumlet.io/images/c_fit,q_auto,w_150,f_auto,h_150/xyy0payx0kdookapcmh3/nature-s-island-multivitamin-for-men-capsule.jpg",
+      brand: "Nature's Island Multivitamin for Men Capsule",
+      name: "bottle of 60 capsules",
+      rupees: "₹",
+      price: 479,
+      rupees2: "₹",
+      price2: 999,
+      offer: "52% OFF"
+    },
+    {
+      avatar:
+        "https://onemg.gumlet.io/images/c_fit,q_auto,w_150,f_auto,h_150/olumjmbboursowikpgh9/tata-1mg-vitamin-b-complex-capsules.jpg",
+      brand: "Tata 1mg Vitamin B Complex Capsules",
+      name: "bottle of 60 capsules",
+      rupees: "₹",
+      price: 448,
+      rupees2: "₹",
+      price2: 595,
+      offer: "24% OFF"
+    },
+    {
+      avatar:
+        "https://onemg.gumlet.io/images/c_fit,q_auto,w_150,f_auto,h_150/dgcszilwgcv0dzorjzdk/muscleblaze-mb-vite-multivitamin-tablet.jpg",
+      brand: "MuscleBlaze MB-Vite Multivitamin Tablet",
+      name: "bottle of 60 capsules",
+      rupees: "₹",
+      price: 529,
+      rupees2: "₹",
+      price2: 699,
+      offer: "24% OFF"
+    },
+    {
+      avatar:
+        "https://onemg.gumlet.io/images/c_fit,q_auto,w_150,f_auto,h_150/biudezibjdr0ejvz8onl/patanjali-nutrela-daily-active-capsule.jpg",
+      brand: "Patanjali Nutrela Daily Active Capsule",
+      name: "bottle of 30 capsules",
+      rupees: "₹",
+      price: 400,
+      rupees2: "₹",
+      price2: 799,
+      offer: "49% OFF"
+    },
+    {
+      avatar:
+        "https://onemg.gumlet.io/images/c_fit,q_auto,w_150,f_auto,h_150/u2f4gd5o673kvaixlr2k/wellbeing-nutrition-slow-multi-for-him-capsule.jpg",
+      brand: "Wellbeing Nutrition Slow Multi for Him Capsule",
+      name: "bottle of 60 capsules",
+      rupees: "₹",
+      price: 1539,
+      rupees2: "₹",
+      price2: 2199,
+      offer: "30% OFF"
+    },
+    {
+      avatar:
+        "https://onemg.gumlet.io/images/c_fit,q_auto,w_150,f_auto,h_150/m6j6dbcf8ht96zbfqjuf/revital-h-capsule.jpg",
+      brand: "Revital H Capsule",
+      name: "bottle of 30 soft gelatin capsules",
+      rupees: "₹",
+      price: 273,
+      rupees2: "₹",
+      price2: 310,
+      offer: "12% OFF"
+    },
+    {
+      avatar:
+        "https://onemg.gumlet.io/images/c_fit,q_auto,w_150,f_auto,h_150/a7xr3jsvuophvvsn7yua/healthvit-cenvitan-men-multivitamin-multimineral-tablet.jpg",
+      brand: "HealthVit Cenvitan Men Multivitamin & Multimineral Tablet",
+      name: "bottle of 60 capsules",
+      rupees: "₹",
+      price: 335,
+      rupees2: "₹",
+      price2: 500,
+      offer: "33% OFF"
+    },
+    {
+      avatar:
+        "https://onemg.gumlet.io/images/c_fit,q_auto,w_150,f_auto,h_150/ktincwpwau6hhhoyabfp/swisse-ultivite-men-s-multivitamin-tablet.jpg",
+      brand: "Swisse Ultivite Men's Multivitamin Tablet",
+      name: "bottle of 30 capsules",
+      rupees: "₹",
+      price: 974,
+      rupees2: "₹",
+      price2: 1299,
+      offer: "10% OFF"
+    },
+    {
+      avatar:
+        "https://onemg.gumlet.io/images/c_fit,q_auto,w_150,f_auto,h_150/wkkptpw7ctbzmwo73mrv/wellbeing-nutrition-slow-multi-for-him-50-capsule.jpg",
+      brand: "Wellbeing Nutrition Slow Multi for Him 50+ Capsule",
+      name: "bottle of 60 capsules",
+      rupees: "₹",
+      price: 1539,
+      rupees2: "₹",
+      price2: 2199,
+      offer: "30% OFF"
+    },
+  
+    {
+      avatar:
+        "https://onemg.gumlet.io/images/c_fit,q_auto,w_150,f_auto,h_150/pk84nhxco4mzoobfeqpr/multivite-gold-daily-health-supplement-softgels.jpg",
+      brand: "Multivite Gold Daily Health Supplement Softgels",
+      name: "strip of 15 soft gelatin capsules",
+      rupees: "₹",
+      price: 230,
+      rupees2: "₹",
+      price2: 264,
+      offer: "13% OFF"
+    },
+    {
+      avatar:
+        "https://images.bewakoof.com/t320/busy-doin-nothing-half-sleeve-t-shirt-287587-1632149262-4.jpg",
+      brand: "Bewakoof",
+      name: "bottle of 60 capsules",
+      rupees: "₹",
+      price: 299,
+      rupees2: "₹",
+      price2: 1099,
+      offer: "10% OFF"
+    },
+    {
+      avatar:
+        "https://onemg.gumlet.io/images/c_fit,q_auto,w_150,f_auto,h_150/cropped/qz7pwtyehvdnoj9piuux/absolut-3g-soft-gelatin-capsule.jpg",
+      brand: "Absolut 3G Soft Gelatin Capsule",
+      name: "strip of 10 soft gelatin capsules",
+      rupees: "₹",
+      price: 170,
+      rupees2: "₹",
+      price2: 196,
+      offer: "13% OFF"
+    },
+    {
+      avatar:
+        "https://onemg.gumlet.io/images/c_fit,q_auto,w_150,f_auto,h_150/grcvrktohcvunrfvytzd/healthkart-hk-vitals-multivitamin-multimineral-amino-acids-taurine-ginseng-extract-tablet.jpg",
+      brand: "HealthKart HK Vitals Multivitamin Multimineral, Amino Acids ,Taurine",
+      name: "bottle of 90 capsules",
+      rupees: "₹",
+      price: 549,
+      rupees2: "₹",
+      price2: 950,
+      offer: "42% OFF"
+    },
+    {
+      avatar:
+        "https://onemg.gumlet.io/images/c_fit,q_auto,w_150,f_auto,h_150/aywb3kusfcv1tnyyt8qy/dr.-morepen-multi-vitamin-men-with-omega-3-herbs-natural-energy-immunity-booster-tablet.jpg",
+      brand: "Dr. Morepen Multi Vitamin Men with Omega 3 & Herbs, Natural Energy",
+      name: "bottle of 60 capsules",
+      rupees: "₹",
+      price: 749,
+      rupees2: "₹",
+      price2: 999,
+      offer: "25% OFF"
+    },
+    {
+      avatar:
+        "https://onemg.gumlet.io/images/c_fit,q_auto,w_150,f_auto,h_150/spizxusftb2bun7ku0ic/multivite-fm-omega-multimineral-multivitamin-supplement-capsule.jpg",
+      brand: "Multivite FM Omega Multimineral & Multivitamin Supplement Capsule",
+      name: "bottle of 30 capsules",
+      rupees: "₹",
+      price: 84,
+      rupees2: "₹",
+      price2: 98,
+      offer: "14% OFF"
+    },
+  
+  
+  ];
+  function handlePriceSort() {
+    var selected = document.querySelector("#sortPrice").value;
+    if (selected == "HTL") {
+      fashionData.sort(function (a, b) {
+        return b.price - a.price;
+      });
+      displayData(fashionData)
+    }
+    if (selected == "LTH") {
+      fashionData.sort(function (a, b) {
+        return a.price - b.price;
+      });
+      displayData(fashionData)
+    }
+    if(selected=="popular"){
+      fashionData.sort(function(a){
+        if(a.price>300)
+        {
+          return -1
+        }
+        if(a.price<300){
+          return 1;
+        }
+        else{
+          0;
+        }
+      });
+      displayData(fashionData)
+    }
+  }
+  
+  //fashionArr=JSON.parse(localStorage.getItem("dataone"))||[];
+  
+  displayData(fashionData)
+  //console.log (fashionArr)
+  
+  
+  function displayData(data) {
+    let container = document.getElementById("container");
+    container.innerHTML=null
+    fashionData.forEach(function (elem) {
+      //console.log (elem.name)
+      var box = document.createElement("div");
+      box.setAttribute("class","box_class")
+      
+      var fashion_avatar = document.createElement("img");
+      fashion_avatar.setAttribute("src", elem.avatar);
+      fashion_avatar.setAttribute("class","product_img2")
+      fashion_avatar.style.cursor = "pointer";
+      fashion_avatar.addEventListener("click", function () {
+        press(elem);
+      })
+
+      var fashion_brand = document.createElement("p");
+      fashion_brand.innerText = elem.brand;
+      fashion_brand.setAttribute("class","brandss")
+
+      var fashion_name = document.createElement("p");
+      fashion_name.innerText = elem.name;
+      fashion_name.setAttribute("class","name2")
+
+      
+  
+      
+      var div6 = document.createElement("div");
+      div6.setAttribute("class","div6")
+      var div61 = document.createElement("div");
+      var div63 = document.createElement("div");
+      var div66 = document.createElement("div");
+      var fashion_rupees1 = document.createElement("p");
+      fashion_rupees1.innerText ="MRP";
+      fashion_rupees1.setAttribute("class","rupees1")
+      div66.append(fashion_rupees1)
+      var div67 = document.createElement("div");
+      var fashion_rupees2 = document.createElement("p");
+      fashion_rupees2.innerText =elem.rupees2;
+      fashion_rupees2.setAttribute("class","rupees_paisa")
+      div67.append(fashion_rupees2)
+      div61.append(div66,div67)
+      div61.setAttribute("class","div61")
+      var div62 = document.createElement("div");
+      var fashion_price2 = document.createElement("p");
+      fashion_price2.innerText = elem.price2;
+      fashion_price2.setAttribute("class","rupees_paisa")
+      div62.append(fashion_price2)
+      div63.append(div61,div62)
+      div63.setAttribute("class","div63")
+      var div64 = document.createElement("div");
+      var fashion_offer = document.createElement("h6");
+      fashion_offer.innerText = elem.offer;
+      fashion_offer.setAttribute("class","fashion_offer");
+      div64.append(fashion_offer)
+      div6.append(div63,div64)
+
+      var div111 = document.createElement("div");
+      div111.setAttribute("class","div111");
+      var div11 = document.createElement("div");
+      div11.setAttribute("class","div111");
+      var div1 = document.createElement("div");
+      var fashion_price = document.createElement("h3");
+      fashion_price.innerText = elem.price;
+      fashion_price.style.color = "black";
+      div1.append(fashion_price)
+      var div5 = document.createElement("div");
+      var fashion_rupees = document.createElement("h3");
+      fashion_rupees.innerText = elem.rupees;
+      fashion_rupees.style.color = "black";
+      div5.append(fashion_rupees)
+      div11.append(div5,div1)
+      var div55 = document.createElement("div");
+      var button23 = document.createElement("button");
+      button23.innerText="ADD";
+      button23.setAttribute("class","add_button2")
+      button23.addEventListener("click", function () {
+        addButtonPress(elem);
+      })
+      div55.append(button23);
+      div111.append(div11,div55);
+      
+      box.append(fashion_avatar, fashion_brand, fashion_name, div6,div111);
+      container.append(box);
+  
+    });
+  }
+  function addButtonPress(elem) {
+    document.querySelector("#container").innerHTML = ""
+    let s = JSON.parse(localStorage.getItem("dataone")) || [];
+    // var product = [];
+    // fashionArr.push(elem);
+    // product.push(elem)
+    s.push(elem);
+    localStorage.setItem("dataone", JSON.stringify(s));
+    console.log(s)
+    window.location.href = "./vitamin_product.html";
+  }
+  
+  function press(elem) {
+    document.querySelector("#container").innerHTML = ""
+    let s = JSON.parse(localStorage.getItem("dataone")) || [];
+    // var product = [];
+    // fashionArr.push(elem);
+    // product.push(elem)
+    s.push(elem);
+    localStorage.setItem("dataone", JSON.stringify(s));
+    console.log(s)
+    window.location.href = "../Product_display&cart/index.html";
+  }
+  // let userdata=JSON.parse(localStorage.getItem('user'));
+  // // console.log(userdata.username);
+  // if(userdata!=null){
+  // document.querySelector("#username").innerText=`Hi, ${userdata.username}`;
+  // document.querySelector('#signin').style.display="none"
+  // document.querySelector('#heart').style.display="none"
+  // }
