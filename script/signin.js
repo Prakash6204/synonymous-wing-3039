5f6da2021d04d5782a97eb61f1e42a075c346ed2
PKmasai <prakashorious@gmail.com>
2022-08-26 16:34:50 +0530

    my login

--- script/signin.js
+++ script/signin.js
@@ -66,3 +66,47 @@ resume = () => {
     plusSlides(slideIndex);
   }, 3000);
 };
+
+
+
+
+// let gogl = document.querySelector("#gogl");
+// gogl.addEventListener("click",function(){
+//     window.location.href = "signup.html"
+// })
+let form = document.querySelector(".form");
+let login = document.querySelector("#login");
+    let userData = JSON.parse(localStorage.getItem("userData")) || [];
+    login.addEventListener("click",function(event){
+        event.preventDefault();
+        let data = {
+            email:form.mobile.value,
+            password:form.password.value,
+        }
+
+        if(checkSignin(data.email,data.password)===true){
+            alert("Sign in Successful");
+            window.location.href="dashboard.html"
+            name(data.email)
+        }else{
+            alert("Wrong Username or Password");
+            window.location.href="signup.html"
+        }
+    })
+
+    function checkSignin(mobile,password){
+        let filter = userData.filter(function(element){
+            return element.mobile === mobile && element.password === password;
+        })
+        if(filter.length>0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    let name = (mobile)=>{
+        userData.forEach((element) => {
+            if(element.mobile == mobile)
+            localStorage.setItem("Name_on_Dashboard", element.name)    
+        });
+    }
